7bcb36e3362edaebceb3bc7525cf7867080dceab
jiangh_80 <jianghao_code@163.com>
2019-10-09 10:18:52 +0800

    城市数据类、天气界面标题、逐小时预测UI

--- app/src/main/java/com/wiker/framework/activity/BaseActivity.java
+++ app/src/main/java/com/wiker/framework/activity/BaseActivity.java
@@ -1,8 +1,15 @@
 package com.wiker.framework.activity;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.IOException;
 
 import butterknife.ButterKnife;
 
@@ -19,6 +26,11 @@ public abstract class BaseActivity extends AppCompatActivity {
 
         setContentView(getLayout());
         ButterKnife.bind(this);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
         initView();
         initData();
     }
--- app/src/main/java/com/wiker/framework/util/DateUtil.java
+++ app/src/main/java/com/wiker/framework/util/DateUtil.java
@@ -1,6 +1,8 @@
 package com.wiker.framework.util;
 
+import java.text.ParsePosition;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
@@ -13,5 +15,17 @@ public class DateUtil {
     public static String date2String(Date date, String format) {
         SimpleDateFormat mSimpleDateFormat = new SimpleDateFormat(format, Locale.US);
         return mSimpleDateFormat.format(date);
+    }
+
+    public static Date string2Date(String strDate) {
+        SimpleDateFormat mSimpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US);
+        return mSimpleDateFormat.parse(strDate, new ParsePosition(0));
+    }
+
+    public static String getHour(String strDate) {
+        Date mDate = string2Date(strDate);
+        Calendar mCalendar = Calendar.getInstance();
+        mCalendar.setTime(mDate);
+        return mCalendar.get(Calendar.HOUR_OF_DAY) + " 时";
     }
 }
